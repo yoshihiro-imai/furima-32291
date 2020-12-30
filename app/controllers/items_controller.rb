c0944a7f448e27c53be9c591b23a3efcd050d520
yoshihiro-imai <75416155+yoshihiro-imai@users.noreply.github.com>
2020-12-30 19:01:38 +0900

    コンフリクトの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,9 @@
 class ItemsController < ApplicationController #アクション定義
 
-
   def index
   end
 
+  def new
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do #ルーティング設定
 
+  devise_for :users
   root to: "items#index"
+  resources :items, only: [:index,:new]
 
 end
