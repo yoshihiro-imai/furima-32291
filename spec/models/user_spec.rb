d7ddbd0e823d91524d7307104b90d418e276ffab
yoshihiro-imai <75416155+yoshihiro-imai@users.noreply.github.com>
2020-12-30 14:41:48 +0900

    テストコード修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -120,17 +120,4 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Birthday can't be blank")
     end
   end
-
-    describe "トップページ" do
-
-    it "ログアウト状態では、ヘッダーに新規登録/ログインボタンが表示されること" do
-    end
-    it "ログイン状態では、ヘッダーにユーザーのニックネーム/ログアウトボタンが表示されること" do
-    end
-    it "ヘッダーの新規登録/ログインボタンをクリックすることで、各ページに遷移できること" do
-    end
-    it "ヘッダーのログアウトボタンをクリックすることで、ログアウトができることg" do
-    end
-
-  end
 end
